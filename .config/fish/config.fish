9b9be8202f536fe7fb79b19ab4b8bdb72a500fd7
shreyasrama <shreyas.rama1@gmail.com>
2026-02-12 16:36:05 +1300

    update AI settings and add pnpm global

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,10 @@ end
 
 set -xg PATH /opt/local/bin /opt/local/sbin $PATH
 
+
+# pnpm
+set -gx PNPM_HOME "/Users/shreyas/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
